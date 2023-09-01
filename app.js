13dcf62cf46fc64d9dcdd95ddaa80b1d9edba9da
shaik Asif <smd.20sa@gmail.com>
2023-09-02 02:38:05 +0530

    half purchase done

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoutes = require("./routes/User");
 const expenseRouter = require("./routes/expense");
+const purchaseRoutes = require("./routes/purchase");
 const sequelize = require("./config/config");
-const User = require("./models/user");
+const User = require("./models/User");
 const Expense = require("./models/expenses");
+const Order = require("./models/orders");
 const app = express();
 
 app.use(bodyParser.json());
@@ -13,10 +15,12 @@ app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/expense", expenseRouter);
+app.use("/api/purchase", purchaseRoutes);
 User.hasMany(Expense);
 // Expense.belongsTo(User);
 Expense.belongsTo(User, { foreignKey: "UserId" });
-
+User.hasMany(Order);
+Order.belongsTo(User);
 sequelize
   .sync()
   .then(() => {
--- controllers/user.js
+++ controllers/user.js
@@ -113,4 +113,5 @@ const login = async (req, res) => {
 module.exports = {
   signup,
   login,
+  generateAccessToken,
 };
