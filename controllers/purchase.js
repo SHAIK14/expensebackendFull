3306ac0d4b9de8c12a1a9aa75c70dabf0c817a57
shaik Asif <smd.20sa@gmail.com>
2023-09-06 15:53:07 +0530

    done with razorpay

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -5,45 +5,70 @@ const userController = require("./user");
 const purchasepremium = async (req, res) => {
   try {
     const rzp = new Razorpay({
-      key_id: "rzp_test_dAe2pjaqtsuO1x", // Replace with your Razorpay key_id
-      key_secret: "5jHTe2aF3APZkAWdLr3wu1ME", // Replace with your Razorpay key_secret
+      key_id: "rzp_test_obigLiSPDcXxSM", // Replace with your Razorpay key_id
+      key_secret: "IQ4M1RyNWUSdM5wIqYxvgpKX", // Replace with your Razorpay key_secret
     });
 
     const amount = 2500;
+    const currency = "INR";
 
-    const order = await rzp.orders.create({ amount, currency: "INR" });
-    await req.user.createOrder({ orderid: order.id, status: "PENDING" });
-    return res.status(201).json({ order, key_id: rzp.key_id, amount });
-  } catch (err) {
-    console.log(err);
-    res.status(403).json({ message: "Something went wrong", error: err });
+    const order = await rzp.orders.create({ amount, currency });
+    await req.user.createOrder({ orderid: order.id, status: order.status });
+    return res
+      .status(201)
+      .json({ order_id: order.id, key_id: rzp.key_id, amount, currency });
+  } catch (error) {
+    console.log("Error creating order:", error);
+    res.status(500).json({ error: "Failed to create order" });
   }
 };
 const updateTransactionStatus = async (req, res) => {
   try {
+    console.log("Request Headers:", req.headers);
+    console.log("Request Body:", req.body);
+
+    console.log("in update trasction function backend", req.body);
     const userId = req.user.id;
     const { payment_id, order_id } = req.body;
-    const order = await Order.findOne({ where: { orderid: order_id } }); //2
-    const promise1 = order.update({
-      paymentid: payment_id,
-      status: "SUCCESSFUL",
-    });
-    const promise2 = req.user.update({ ispremiumuser: true });
+    if (!order_id) {
+      console.log("Order ID is missing in the request.");
+      return res.status(400).json({ error: "Order ID is missing" });
+    }
 
-    Promise.all([promise1, promise2])
-      .then(() => {
-        return res.status(202).json({
-          sucess: true,
-          message: "Transaction Successful",
-          token: userController.generateAccessToken(userId, undefined, true),
-        });
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
-  } catch (err) {
-    console.log(err);
-    res.status(403).json({ errpr: err, message: "Sometghing went wrong" });
+    const order = await Order.findOne({ where: { orderid: order_id, userId } });
+    if (!order) {
+      console.log("Order not found for the user.");
+      return;
+    }
+    req.user.ispremium = true;
+    await req.user.save();
+    order.status = "paid";
+    order["payment_id"] = payment_id;
+    //  Object.assign(order, {status:'paid' , payment_id:razorpay_payment_id}); // Merge the updatedData into the order object
+    await order.save(); // Save the updated order to the database
+    res.json({ message: "Payment successful", isPremium: req.user.ispremium });
+
+    //2
+    // const promise1 = order.update({
+    //   paymentid: payment_id,
+    //   status: "SUCCESSFUL",
+    // });
+    // const promise2 = req.user.update({ ispremiumuser: true });
+
+    // Promise.all([promise1, promise2])
+    // .then(() => {
+    //   return res.status(202).json({
+    //     sucess: true,
+    //     message: "Transaction Successful",
+    //     token: userController.generateAccessToken(userId, undefined, true),
+    //   });
+    // })
+    // .catch((error) => {
+    //   throw new Error(error);
+    // });
+  } catch (error) {
+    console.log("Error verifying payment:", error);
+    res.status(500).json({ error: "Failed to verify payment" });
   }
 };
 
