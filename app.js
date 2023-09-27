e1186c6e26f91054682b952e876c55e5b52817d9
shaik Asif <smd.20sa@gmail.com>
2023-09-27 12:54:10 +0530

    .env

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
 const userRoutes = require("./routes/User");
 const expenseRouter = require("./routes/expense");
 const purchaseRoutes = require("./routes/purchase");
 const premiumFeatureRoutes = require("./routes/premiumFeature");
 const resetPasswordRoutes = require("./routes/resetpassword");
-
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
 const sequelize = require("./config/config");
 const User = require("./models/user");
 const Expense = require("./models/expenses");
@@ -23,6 +31,9 @@ app.use("/api/expense", expenseRouter);
 app.use("/api/purchase", purchaseRoutes);
 app.use("/api/premium", premiumFeatureRoutes);
 app.use("/api/password", resetPasswordRoutes);
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
 User.hasMany(Expense);
 // Expense.belongsTo(User);
 Expense.belongsTo(User, { foreignKey: "UserId" });
