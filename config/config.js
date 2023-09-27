959d93f6fa9f760b1998a558536fee595d972fa7
shaik Asif <smd.20sa@gmail.com>
2023-09-27 15:17:04 +0530

    .c

--- config/config.js
+++ config/config.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('expensefull', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const Sequelize = require("sequelize");
+require("dotenv").config();
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
